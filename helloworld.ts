8e4c71bcbca089b01ddb3a97d67c835b9205ad62
Antje Sommer <antje.som@gmail.com>
2019-06-23 19:51:07 +0200

    add remove method to helloworld tooltip class

--- helloworld.ts
+++ helloworld.ts
@@ -1,20 +1,30 @@
 class Helloworld extends HTMLElement {
+    tooltipContainer: any;
+
     constructor() {
         super();
+        this.tooltipContainer;
     }
+
     connectedCallback() {
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?) ';
-        tooltipIcon.addEventListener('mouseenter', this.showToolTip.bind(this)); // eventlistener pointer
+        tooltipIcon.addEventListener('mouseenter', this.showToolTip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this.hideTooltip.bind(this)); // eventlistener pointer
         this.appendChild(tooltipIcon);
         console.log('hello world');
     }
 
     private showToolTip() {
-        const tooltipContainer:any = document.createElement('div');
-        tooltipContainer.textContent = 'Hello World Webcomponent!';
-        this.appendChild(tooltipContainer);
+        this.tooltipContainer = document.createElement('div');
+        this.tooltipContainer.textContent = 'Hello World Webcomponent!';
+        this.appendChild(this.tooltipContainer);
     }
+
+    private hideTooltip() {
+        this.removeChild(this.tooltipContainer);
+    }
+
 }
 
 
