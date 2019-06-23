b9b903c9d65fc88885d33a98c4814f7464749659
Antje Sommer <antje.som@gmail.com>
2019-06-23 19:23:49 +0200

    create webcomponent helloworld tooltip

--- helloworld.ts
+++ helloworld.ts
@@ -4,9 +4,16 @@ class Helloworld extends HTMLElement {
     }
     connectedCallback() {
         const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' ?';
+        tooltipIcon.textContent = ' (?) ';
+        tooltipIcon.addEventListener('mouseenter', this.showToolTip.bind(this)); // eventlistener pointer
         this.appendChild(tooltipIcon);
         console.log('hello world');
+    }
+
+    private showToolTip() {
+        const tooltipContainer:any = document.createElement('div');
+        tooltipContainer.textContent = 'Hello World Webcomponent!';
+        this.appendChild(tooltipContainer);
     }
 }
 
