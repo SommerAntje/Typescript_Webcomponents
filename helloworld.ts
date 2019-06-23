473941446dcc80959809bc1d7f6b11d5575230e6
Antje Sommer <antje.som@gmail.com>
2019-06-23 19:53:31 +0200

    add second tooltip content remove console.log

--- helloworld.ts
+++ helloworld.ts
@@ -12,7 +12,6 @@ class Helloworld extends HTMLElement {
         tooltipIcon.addEventListener('mouseenter', this.showToolTip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this.hideTooltip.bind(this)); // eventlistener pointer
         this.appendChild(tooltipIcon);
-        console.log('hello world');
     }
 
     private showToolTip() {
